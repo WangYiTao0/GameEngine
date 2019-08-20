0758aa312e7d8f82b800a8fcf60b6619f745a90b
WangYiTao0 <as5818956@gmail.com>
2019-08-20 19:03:11 +0900

    Revert "mouse capture test code"
    This reverts commit 46096276dee263a3d4e98a8ab76ad6bc2d3b6a14.

--- GameEngine/WinMain.cpp
+++ GameEngine/WinMain.cpp
@@ -17,24 +17,7 @@ int CALLBACK WinMain(
 			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
-			// test code
-			while (!wnd.mouse.IsEmpty())
-			{
-				const auto e = wnd.mouse.Read();
-				switch (e.GetType())
-				{
-				case Mouse::Event::Type::Leave:
-					wnd.SetTitle("Gone!");
-					break;
-				case Mouse::Event::Type::Move:
-				{
-					std::ostringstream oss;
-					oss << "Mouse moved to (" << e.GetPosX() << "," << e.GetPosY() << ")";
-					wnd.SetTitle(oss.str());
-				}
-				break;
-				}
-			}
+			
 		}
 
 		// check if GetMessage call itself borked
