02433cbd034e64e792ad0de34500de13b71b5c94
WangYiTao0 <as5818956@gmail.com>
2019-08-20 18:32:55 +0900

    KeyBoard done

--- GameEngine/Window.h
+++ GameEngine/Window.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ChiliWin.h"
 #include "ChiliException.h"
+#include "Keyboard.h"
 class Window
 {
 public:
@@ -45,6 +46,8 @@ private:
 	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+public:
+	Keyboard kbd;
 private:
 	int width;
 	int height;
