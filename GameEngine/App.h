69c8ecae1ffecfdb1e6d7de7041264d177ed9c4a
Wang <as5818956@gmail.com>
2019-08-26 01:13:46 +0900

    bad camera

--- GameEngine/App.h
+++ GameEngine/App.h
@@ -18,8 +18,8 @@ public:
 private:
 	void DoFrame();
 	void ShowImguiDemoWindow();
-	void ShowRawInputWindow();
 private:
+	bool showDemoWindow = false;
 	int x = 0, y = 0;
 	ImguiManager imgui;
 	Window wnd;
--- GameEngine/Camera.h
+++ GameEngine/Camera.h
@@ -4,15 +4,24 @@
 class Camera
 {
 public:
+	Camera();
 	DirectX::XMMATRIX GetMatrix() const noexcept;
 	void SpawnControlWindow() noexcept;
 	void Reset() noexcept;
+	void Rotate(float dx, float dy) noexcept;
+	void Translate(DirectX::XMFLOAT3 translation) noexcept;
 private:
-	float r = 20.0f; //distance to the origin
-	float theta = 0.0f;//rotation the camera around origin
-	float phi = 0.0f;
-	// rotation camera
-	float pitch = 0.0f;
-	float yaw = 0.0f;
-	float roll = 0.0f;
+	//float r = 20.0f; //distance to the origin
+	//float theta = 0.0f;//rotation the camera around origin
+	//float phi = 0.0f;
+	//// rotation camera
+	//float pitch = 0.0f;
+	//float yaw = 0.0f;
+	//float roll = 0.0f;
+
+	DirectX::XMFLOAT3 pos;
+	float pitch;
+	float yaw;
+	static constexpr float travelSpeed = 12.0f;
+	static constexpr float rotationSpeed = 0.004f;
 };
