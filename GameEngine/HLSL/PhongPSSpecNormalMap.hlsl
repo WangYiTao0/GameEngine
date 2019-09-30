7bd4b6cb56de970712fd95eaef07c583c792934a
Wang <as5818956@gmail.com>
2019-09-30 12:48:18 +0900

    shader function

--- GameEngine/HLSL/PhongPSSpecNormalMap.hlsl
+++ GameEngine/HLSL/PhongPSSpecNormalMap.hlsl
@@ -25,42 +25,79 @@ Texture2D nmap;
 
 SamplerState splr;
 
-float3 MapNormalViewSpace(const float3 tan, const float3 bitan, const float3 viewNormal, const float2 tc, Texture2D nmap, SamplerState sptr)
+float3 MapNormal(
+    const in float3 tan, 
+    const in float3 bitan, 
+    const in float3 normal, 
+    const in float2 tc, 
+    uniform Texture2D nmap, 
+    uniform SamplerState sptr)
 {
-    // build the tranform (rotation) into tangent space
-    const float3x3 tanToView = float3x3(
-            normalize(tan),
-            normalize(bitan),
-            normalize(viewNormal)
-        );
-    //sample and unpack the normal from texture into tangent space
+    // build the tranform (rotation) into same space as tan/bitan/normal s(target space)
+    const float3x3 tanToTarget = float3x3(tan, bitan, normal);
+    //sample and unpack the normal from texture into target space
     const float3 normalSample = nmap.Sample(splr, tc).xyz;
     const float3 tanNormal = normalSample * 2.0f - 1.0f;
-    //bring normal from tanspace into view space
+    //bring normal from tanspace into target space
 
-    return normalize(mul(tanNormal, tanToView));
+    return normalize(mul(tanNormal, tanToTarget));
 
 }
 
-
-float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tan : Tangent, float3 bitan : Bitangent, float2 tc : Texcoord) : SV_Target
+float Attenuate(
+    uniform float attConst, 
+    uniform float attLin, 
+    uniform float attQuad, 
+    const in float distFragToL)
 {
+    return 1.0f / (attConst + attLin * distFragToL + attQuad * (distFragToL * distFragToL));
+}
+
+float3 Diffuse(
+    uniform float3 diffuseColor,
+    uniform float diffuseIntensity,
+    const in float att,
+    const in float3 viewDirFragToL,
+    const in float3 viewNormal
+  )
+{
+    return diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToL, viewNormal));
+}
+
+float3 Speculate(
+    const in float3 specularColor,
+    uniform float specularIntensity,
+    const in float3 viewNormal,
+    const in float3 viewFragToL,
+    const in float3 viewPos,
+    const in float att,
+    const in float specularPower)
+{
+    // calculate reflected light vector
+    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
+    const float3 r = normalize(w * 2.0f - viewFragToL);
+    // vector from camera to fragment (in view space)
+    const float3 viewCamToFrag = normalize(viewPos);
+    // calculate specular component color based on angle between
+    // viewing vector and reflection vector, narrow with power function
+    return att * specularColor * specularIntensity * pow(max(0.0f, dot(-r, viewCamToFrag)), specularPower);
+}
+
+float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 viewTan : Tangent, float3 viewBitan : Bitangent, float2 tc : Texcoord) : SV_Target
+{
+    // normalize the mesh normal
+    viewNormal = normalize(viewNormal);
+
     // sample normal from map if normal mapping enabled
     if (normalMapEnabled)
     {
-        viewNormal = MapNormalViewSpace(tan, bitan, viewNormal, tc, nmap, splr);
+        viewNormal = MapNormal(normalize(viewTan), normalize(viewBitan), viewNormal, tc, nmap, splr);
     }
 	// fragment to light vector data
     const float3 viewFragToL = viewLightPos - viewPos;
     const float distFragToL = length(viewFragToL);
     const float3 viewDirFragToL = viewFragToL / distFragToL;
-	// attenuation
-    const float att = 1.0f / (attConst + attLin * distFragToL + attQuad * (distFragToL * distFragToL));
-	// diffuse intensity
-    const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToL, viewNormal));
-	// reflected light vector
-    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
-    const float3 r = w * 2.0f - viewFragToL;
+    //specular parameter determination (mapped or uniform)
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
     float3 specularReflectionColor;
     float specularPower = specularPowerConst;
@@ -79,8 +116,13 @@ float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tan : 
     {
         specularPower = specularColor;
     }
+    // attenuation
+    const float att = Attenuate(attConst, attLin, attQuad, distFragToL);
+	// diffuse light
+    const float3 diffuse = Diffuse(diffuseColor, diffuseIntensity, att, viewDirFragToL, viewNormal);
 
-    const float3 specular = att * (diffuseColor * diffuseIntensity) * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
-	// final color
-    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular * specularReflectionColor), 1.0f);
+    // specular reflected
+    const float3 specularReflected = Speculate(specularReflectionColor, 1.0f, viewNormal, viewFragToL, viewPos, att, specularPower);
+    // final color attenuate diffuse & ambient by diffuse texture color and add specular reflected
+    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specularReflected), 1.0f);
 }
